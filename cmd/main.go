872961e65cf6790ec3dc4f0662137295e902c366
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-22 19:31:07 +0300

    Добавлен recover middleware

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/viacheslav-korobeynikov/vacancy-site/config"
 	"github.com/viacheslav-korobeynikov/vacancy-site/internal/home"
 )
@@ -13,6 +14,8 @@ func main() {
 	dbConf := config.NewDatabaseConfig() // Вызов конфигурации БД
 	log.Println(dbConf)
 	app := fiber.New() // Создание инстанса приложения Fiber
+
+	app.Use(recover.New()) // Middleware, который перезапускает приложение в случае, если произошел вызов panic
 
 	home.NewHandler(app) // Добавили зависимость с хэндлером для главной страницы
 
