73790930f26cfbbdca8045dfa836ce5945052ebb
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-22 18:38:09 +0300

    Создан простой сервер на Fiber

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Start")
+	app := fiber.New() // Создание инстанса приложения Fiber
+
+	//Простейший хэндлер
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello") // Возвращаем ответ
+	})
+
+	app.Listen(":3000") //Настраиваем порт, который будем слушать
 }
