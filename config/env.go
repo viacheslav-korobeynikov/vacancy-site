6f149874740375e18106cc91e55ddfb8f5b604b6
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-22 19:16:20 +0300

    Добавлены функции для работы с дефолтными значениями конфигураций

--- config/env.go
+++ config/env.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -16,6 +17,49 @@ func Init() {
 	log.Println(".env file loaded")
 }
 
+// Работа с дефолтными значениями
+func getString(key, defaultValue string) string {
+	// Читаем из переменной окружения
+	value := os.Getenv(key)
+	// Если значение пустое
+	if value == "" {
+		// Возвращаем дефолтное значение
+		return defaultValue
+	}
+	// Если значение не пустое - возвращаем значение переменной окружения
+	return value
+}
+
+// Работа с числовыми значениями
+func getInt(key string, defaultValue int) int {
+	// Читаем из переменной окружения
+	value := os.Getenv(key)
+	// Конвертируем в int
+	i, err := strconv.Atoi(value)
+	// Если не удалось сконвертировать
+	if err != nil {
+		// Возвращаем дефолтное значение
+		return defaultValue
+	}
+	// Если удалось сконвертировать - возвращаем значение int
+	return i
+}
+
+// Работа с булево значениями
+func getBool(key string, defaultValue bool) bool {
+	// Читаем из переменной окружения
+	value := os.Getenv(key)
+	// Парсим значение bool
+	b, err := strconv.ParseBool(value)
+	// Если не удалось спарсить
+	if err != nil {
+		// Возвращаем дефолтное значение
+		return defaultValue
+	}
+	// Если удалось сконвертировать - возвращаем значение bool
+	return b
+}
+
 // Структура конфигурации БД
 type DatabaseConfig struct {
 	url string
@@ -24,6 +68,6 @@ type DatabaseConfig struct {
 // Извление конфига БД из переменной окружения
 func NewDatabaseConfig() *DatabaseConfig {
 	return &DatabaseConfig{
-		url: os.Getenv("DATABASE_URL"),
+		url: getString("DATABASE_URL", ""),
 	}
 }
