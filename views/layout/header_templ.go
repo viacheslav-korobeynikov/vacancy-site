fa6db2d7dc2a8a7d78be18d7bcdb5bf89fd8d73c
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-23 18:43:53 +0300

    Добавлены стили css для меню

--- views/layout/header_templ.go
+++ views/layout/header_templ.go
@@ -62,6 +62,10 @@ func Header() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = Menu().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
